849cfa6b3ad1b755775b74563ee53c481b804cd4
OctavioYN <49240700+OctavioYN@users.noreply.github.com>
2025-11-13 12:28:41 -0600

    Delete src directory

--- src/main/java/com/resumen/nomina/application/repository/CalculoIndicadorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.resumen.nomina.application.repository;
-
-import com.resumen.nomina.domain.model.Indicador;
-import java.util.List;
-
-public interface CalculoIndicadorRepository {
-    List<Indicador> obtenerIndicadoresPorPeriodos(List<String> periodos);
-}
